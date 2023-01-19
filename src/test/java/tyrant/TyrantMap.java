579f553944e85b4a2ab274b2e0cf88404952d29f
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-19 17:41:31 +0900

    TyrantMap 리팩토링, 최종 test

---
+++ src/test/java/tyrant/TyrantMap.java
@@ -0,0 +1,69 @@
+package tyrant;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+class TyrantMap {
+
+    public static final int OPERATION_PREFIX = 0xC8;
+    public static final int OPERATION_PUT = 0x10;
+    private static final int OPERATION_GET = 0x30;
+    private Socket socket;
+    private DataOutputStream writer;
+    private DataInputStream reader;
+
+    public void open() throws IOException {
+        socket = new Socket("localhost", 1978);
+        writer = new DataOutputStream(socket.getOutputStream());
+        reader = new DataInputStream(socket.getInputStream());
+    }
+    public void put(String key, String value) throws IOException {
+        writeHeader(OPERATION_PUT);
+        writeKeyValue(key, value);
+        verifyStatus();
+    }
+
+    public byte[] get(String key) throws IOException {
+        writeHeader(OPERATION_GET);
+        writeKey(key);
+        verifyStatus();
+        return readResults();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+
+    private byte[] readResults() throws IOException {
+        int length = reader.readInt();
+        byte[] results = new byte[length];
+        reader.read(results);
+        return results;
+    }
+
+    private void writeKey(String key) throws IOException {
+        writer.writeInt(key.length()); //4 byte
+        writer.write(key.getBytes()); //key
+    }
+
+    private void verifyStatus() throws IOException {
+        int status = reader.read();
+        assertThat(status, is(0));
+    }
+
+    private void writeKeyValue(String key, String value) throws IOException {
+        writer.writeInt(key.length()); //4 byte
+        writeKey(value);
+        writer.write(value.getBytes()); //value
+    }
+
+    private void writeHeader(int operationPut) throws IOException {
+        writer.write(OPERATION_PREFIX);
+        writer.write(operationPut);
+    }
+}
--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -2,18 +2,12 @@ package tyrant;
 
 import org.junit.Test;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TyrantTest {
-
-
 
     @Test
     public void getRetrievesWhatWasPut() throws IOException {
@@ -26,51 +20,5 @@ public class TyrantTest {
         tyrantMap.put("key", "value");
         assertThat(tyrantMap.get("key"), is("value".getBytes()));
         tyrantMap.close();
-
-    }
-
-    private class TyrantMap {
-
-        public static final int OPERATION_PREFIX = 0xC8;
-        public static final int OPERATION_PUT = 0x10;
-        private static final int OPERATION_GET = 0x30;
-        private Socket socket;
-        private DataOutputStream writer;
-        private DataInputStream reader;
-
-        public void put(String key, String value) throws IOException {
-
-            writer.write(OPERATION_PREFIX);
-            writer.write(OPERATION_PUT);
-            writer.writeInt(key.length()); //4 byte
-            writer.writeInt(value.length()); //4 byte
-            writer.write(key.getBytes()); //key
-            writer.write(value.getBytes()); //value
-            int status = reader.read();
-            assertThat(status, is(0));
-        }
-
-        private void open() throws IOException {
-            socket = new Socket("localhost", 1978);
-            writer = new DataOutputStream(socket.getOutputStream());
-            reader = new DataInputStream(socket.getInputStream());
-        }
-
-        public void close() throws IOException {
-            socket.close();
-        }
-
-        public byte[] get(String key) throws IOException {
-            writer.write(OPERATION_PREFIX);
-            writer.write(OPERATION_GET);
-            writer.writeInt(key.length()); //4 byte
-            writer.write(key.getBytes()); //key
-            int status = reader.read();
-            assertThat(status, is(0));
-            int length = reader.readInt();
-            byte[] results = new byte[length];
-            reader.read(results);
-            return results;
-        }
     }
 }
