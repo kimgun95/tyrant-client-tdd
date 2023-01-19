86e72df5a5858d41cd14e66ba2756583efe3303d
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-19 19:36:11 +0900

    TyrantTest 리팩토링

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -1,5 +1,7 @@
 package tyrant;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,16 +11,23 @@ import static org.junit.Assert.assertThat;
 
 public class TyrantTest {
 
+    private TyrantMap tyrantMap;
+    @Before
+    public void setUp() throws IOException {
+        tyrantMap = new TyrantMap();
+        tyrantMap.open();
+    }
+    @After
+    public void tearDown() throws IOException {
+        tyrantMap.close();
+    }
     @Test
     public void getRetrievesWhatWasPut() throws IOException {
 //        Tyrant t = new Tyrant();
 //        t.put("key", "value");
 //        assertThat(t.get("key"), is("value"));
 
-        TyrantMap tyrantMap = new TyrantMap();
-        tyrantMap.open();
         tyrantMap.put("key", "value");
         assertThat(tyrantMap.get("key"), is("value".getBytes()));
-        tyrantMap.close();
     }
 }
