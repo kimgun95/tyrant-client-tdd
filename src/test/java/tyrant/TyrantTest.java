3161781c6105a2af81436b9e132de6f6234f4c6e
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-19 16:30:10 +0900

    패킷 write,read 테스트

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -3,7 +3,12 @@ package tyrant;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class TyrantTest {
     @Test
@@ -12,6 +17,24 @@ public class TyrantTest {
 //        t.put("key", "value");
 //        assertThat(t.get("key"), is("value"));
 
-        new Socket("localhost", 1978);
+        Socket socket = new Socket("localhost", 1978);
+        OutputStream writer = socket.getOutputStream();
+        writer.write(0xC8); //OPERATION_PREFIX
+        writer.write(0x10); //OPERATION_PUT
+        writer.write(0);
+        writer.write(0);
+        writer.write(0);
+        writer.write(3); //4 byte key length
+        writer.write(0);
+        writer.write(0);
+        writer.write(0);
+        writer.write(5); //4 byte value length
+        writer.write(new byte[]{'k', 'e', 'y'}); //key
+        writer.write(new byte[]{'v', 'a', 'l', 'u', 'e'}); //value
+
+        InputStream reader = socket.getInputStream();
+        int status = reader.read();
+        assertThat(status, is(0));
+
     }
 }
