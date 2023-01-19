f12d994d29ebabeab1cfb48ace719ed414aae562
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-19 16:42:17 +0900

    tyrantMap put() 리팩토링

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -11,30 +11,48 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TyrantTest {
+
+
+
     @Test
     public void getRetrievesWhatWasPut() throws IOException {
 //        Tyrant t = new Tyrant();
 //        t.put("key", "value");
 //        assertThat(t.get("key"), is("value"));
 
-        Socket socket = new Socket("localhost", 1978);
-        OutputStream writer = socket.getOutputStream();
-        writer.write(0xC8); //OPERATION_PREFIX
-        writer.write(0x10); //OPERATION_PUT
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(3); //4 byte key length
-        writer.write(0);
-        writer.write(0);
-        writer.write(0);
-        writer.write(5); //4 byte value length
-        writer.write(new byte[]{'k', 'e', 'y'}); //key
-        writer.write(new byte[]{'v', 'a', 'l', 'u', 'e'}); //value
+        new TyrantMap().put();
 
-        InputStream reader = socket.getInputStream();
-        int status = reader.read();
-        assertThat(status, is(0));
+    }
 
+    private class TyrantMap {
+
+        public static final int OPERATION_PREFIX = 0xC8;
+        public static final int OPERATION_PUT = 0x10;
+        private Socket socket;
+        private OutputStream writer;
+        private InputStream reader;
+
+        public void put() throws IOException {
+            String key = "key";
+            String value = "value";
+            socket = new Socket("localhost", 1978);
+            writer = socket.getOutputStream();
+            writer.write(OPERATION_PREFIX);
+            writer.write(OPERATION_PUT);
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(3); //4 byte key length
+            writer.write(0);
+            writer.write(0);
+            writer.write(0);
+            writer.write(5); //4 byte value length
+            writer.write(key.getBytes()); //key
+            writer.write(value.getBytes()); //value
+
+            reader = socket.getInputStream();
+            int status = reader.read();
+            assertThat(status, is(0));
+        }
     }
 }
