d89591e8fdd8cf81c7370040b0dbe5a03eb80a75
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-19 17:20:54 +0900

    get() 구현

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -2,6 +2,7 @@ package tyrant;
 
 import org.junit.Test;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,6 +24,7 @@ public class TyrantTest {
         TyrantMap tyrantMap = new TyrantMap();
         tyrantMap.open();
         tyrantMap.put("key", "value");
+        assertThat(tyrantMap.get("key"), is("value".getBytes()));
         tyrantMap.close();
 
     }
@@ -31,9 +33,10 @@ public class TyrantTest {
 
         public static final int OPERATION_PREFIX = 0xC8;
         public static final int OPERATION_PUT = 0x10;
+        private static final int OPERATION_GET = 0x30;
         private Socket socket;
         private DataOutputStream writer;
-        private InputStream reader;
+        private DataInputStream reader;
 
         public void put(String key, String value) throws IOException {
 
@@ -50,11 +53,24 @@ public class TyrantTest {
         private void open() throws IOException {
             socket = new Socket("localhost", 1978);
             writer = new DataOutputStream(socket.getOutputStream());
-            reader = socket.getInputStream();
+            reader = new DataInputStream(socket.getInputStream());
         }
 
         public void close() throws IOException {
             socket.close();
+        }
+
+        public byte[] get(String key) throws IOException {
+            writer.write(OPERATION_PREFIX);
+            writer.write(OPERATION_GET);
+            writer.writeInt(key.length()); //4 byte
+            writer.write(key.getBytes()); //key
+            int status = reader.read();
+            assertThat(status, is(0));
+            int length = reader.readInt();
+            byte[] results = new byte[length];
+            reader.read(results);
+            return results;
         }
     }
 }
