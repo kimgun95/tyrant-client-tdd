158a9e07ed88e90b25e5e3ff6385c8e8559ba9c7
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-01-23 20:49:31 +0900

    socket test

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -5,29 +5,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.Socket;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class TyrantTest {
 
-    private TyrantMap tyrantMap;
-    @Before
-    public void setUp() throws IOException {
-        tyrantMap = new TyrantMap();
-        tyrantMap.open();
-    }
-    @After
-    public void tearDown() throws IOException {
-        tyrantMap.close();
-    }
+//    private TyrantMap tyrantMap;
+//    @Before
+//    public void setUp() throws IOException {
+//        tyrantMap = new TyrantMap();
+//        tyrantMap.open();
+//    }
+//    @After
+//    public void tearDown() throws IOException {
+//        tyrantMap.close();
+//    }
     @Test
     public void getRetrievesWhatWasPut() throws IOException {
 //        Tyrant t = new Tyrant();
 //        t.put("key", "value");
 //        assertThat(t.get("key"), is("value"));
 
-        tyrantMap.put("key", "value");
-        assertThat(tyrantMap.get("key"), is("value".getBytes()));
+        new Socket("localhost", 1978);
+
+//        tyrantMap.put("key", "value");
+//        assertThat(tyrantMap.get("key"), is("value".getBytes()));
     }
 }
